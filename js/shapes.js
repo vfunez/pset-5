959c2d528ccdb0f6ebe8cd73189b67e4cd55e38c
Vanya <vfunez@ucvts.org>
2019-12-12 22:56:43 -0500

    exercise five 🙄

--- js/shapes.js
+++ js/shapes.js
@@ -22,6 +22,7 @@ window.onload = function() {
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
     document.getElementById("triangle").onclick = drawTriangle;
+    document.getElementById("smile").onclick = drawFace;
 }
 
 /*
@@ -173,7 +174,44 @@ do {
  */
 
 const drawFace = function() {
-    // write your exercise 4 code here
+  const ctx = document.getElementById('student-canvas-5').getContext('2d');
+  ctx.clearRect(0, 0, 1024, 512);
+
+  do {
+      let radius = prompt("Radius:");
+      if (radius == null) {
+        break;
+      }
+      if (radius < 32) {
+       alert("Your radius must be at least 32.")
+      }
+      if (radius > 256) {
+       alert("Your smiley face won't fit on the canvas.")
+      }
+      if (isNaN(radius)) {
+       alert("Your radius is not a number.")
+      }
+    } while (radius < 32 || isNaN(radius) || radius > 256)
+
+      let eyes = 0.15 * radius
+      let mouth = 0.7 * radius
+
+  ctx.beginPath();
+  ctx.arc(512, 256, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(512, 256, mouth, 0, Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(512 - 0.4 * radius, 256 - 0.4 * radius, eyes, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(512 + 0.4 * radius, 256 - 0.4 * radius, eyes, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.closePath();
 };
 
 /*
